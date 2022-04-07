7cfd29ed7eee6e306b0099ff8fbb26a08a763cbb
emmlorl <emmlorl@github.com>
2022-04-07 06:33:06 -0700

    ptinting a program and returns 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *Printing a program in c
+ *Using actual gcc program
+ */
+
+int main(void)
+{puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
