37da2497989a65afa11c614075ee8c6fcb083777
emmlorl <emmlorl@github.com>
2022-04-07 06:42:52 -0700

    ptinting a program and returns 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- *Printing a program in c
- *Using actual gcc program
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-{       puts("Programming is like building a multilingual puzzle\n");
+{       puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
