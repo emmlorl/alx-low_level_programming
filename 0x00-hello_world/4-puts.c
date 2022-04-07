7ef4c1a57045899341a498c910e711fe2df2cbb8
emmlorl <emmlorl@github.com>
2022-04-07 06:39:22 -0700

    ptinting a program and returns 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -7,6 +6,6 @@
  */
 
 int main(void)
-{puts("Programming is like building a multilingual puzzle\n");
+{       puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
