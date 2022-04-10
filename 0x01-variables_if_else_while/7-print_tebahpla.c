047df5050ae76205015e6db8e2bf1f7c51ceaf4f
emmlorl <emmlorl@github.com>
2022-04-10 02:30:26 -0700

    priting alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - printing lowercase alpha in reverse order,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
